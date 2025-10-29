8f18b9305eb7d194daa6e65d3c553fb6f785ce5c
Happy Singh <solo.leveler15@gmail.com>
2025-10-29 22:52:53 +0530

    added endpoint for search

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns=[
     path('url/',views.url_list_create_view),
-    path('exercise/',views.exercise_list_create_view)
+    path('exercise/',views.exercise_list_create_view),
+    path('search/',views.search_api_view)
 ]
