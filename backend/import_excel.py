25322d72c035e3dccf323406ebfc36c4a2355cfe
Happy Singh <solo.leveler15@gmail.com>
2025-10-29 13:18:40 +0530

    imported data to database

--- backend/import_excel.py
+++ backend/import_excel.py
@@ -5,15 +5,25 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','home.settings')
 django.setup()
 
 import pandas as pd
-from api.models import Url
+from api.models import Url, Exercise
 
-df = pd.read_csv('../data/backend_test.csv')
-print(df)
+df_url = pd.read_csv('data/backend_test.csv')
+df_exercise = pd.read_csv('data/filename.csv')
 
-for _, row in df.iterrows():
+for _, row in df_url.iterrows():
     Url.objects.create(
-        exercise = row['Exercise'],
+        title = row['Exercise'],
         url = row['URL']
+    
+    )
+    
+for _, row in df_exercise.iterrows():
+    Exercise.objects.create(
+        title = row['Title'],
+        exercise_type = row['Type'],
+        bodypart = row['BodyPart'],
+        equipment = row['Equipment'],
+        level = row['Level']
     )
 
 print('data imported Successfully')
