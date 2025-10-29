46d9b792ecffaccd301faf6dbd116e9255a16811
Happy Singh <solo.leveler15@gmail.com>
2025-10-29 22:52:09 +0530

    created a search view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,9 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from algoliasearch_django import raw_search
+
+
 
 from .models import Url, Exercise
 from .serializers import UrlSerializer, ExerciseSerializer
@@ -20,3 +25,11 @@ class ExerciseListCreateAPIView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         return super().perform_create(serializer)
 exercise_list_create_view = ExerciseListCreateAPIView.as_view()
+
+
+class SearchView(APIView):
+    def get(self, request):
+        query = request.GET.get('q', '')
+        results = raw_search(Exercise, query)
+        return Response(results)
+search_api_view = SearchView.as_view()
