f96abbd0dfa257a11cc717beaef12585fa440e77
Happy Singh <solo.leveler15@gmail.com>
2025-10-30 23:53:01 +0530

    imported data into database

--- backend/import_excel.py
+++ backend/import_excel.py
@@ -4,26 +4,27 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE','home.settings')
 django.setup()
 
+from django.db import transaction
 import pandas as pd
-from api.models import Url, Exercise
+from api.models import Exercise
 
-df_url = pd.read_csv('data/backend_test.csv')
-df_exercise = pd.read_csv('data/filename.csv')
+df_exercise = pd.read_csv('data/Exercise__data.csv')
+print(f"Total rows to import: {len(df_exercise)}")
 
-for _, row in df_url.iterrows():
-    Url.objects.create(
-        title = row['Exercise'],
-        url = row['URL']
-    
-    )
-    
-for _, row in df_exercise.iterrows():
-    Exercise.objects.create(
-        title = row['Title'],
-        exercise_type = row['Type'],
-        bodypart = row['BodyPart'],
-        equipment = row['Equipment'],
-        level = row['Level']
-    )
+count = 0
+with transaction.atomic():
+    for _, row in df_exercise.iterrows():
+        Exercise.objects.create(
+            title = row['Exercise_name'],
+            reps_sets = row['Exercise_count'],
+            exercise_type = row['Type'],
+            bodypart = row['Bodypart'],
+            # equipment = row['Equipment'],
+            level = row['Level'],
+            url = row['YouTube Link'],
+        )
+        count += 1
+        if count % 10 == 0:  # print every 100 rows
+            print(f"Imported {count} rows...")
 
-print('data imported Successfully')
+print("Import completed.")
